dd94102a989f85ff2e11500a9b68def7de6c862f
Mark Ebert <markebert@users.noreply.github.com>
2024-01-27 10:11:30 -0500

    Basic shooter code

--- src/main/java/frc/robot/RobotProperties.java
+++ src/main/java/frc/robot/RobotProperties.java
@@ -31,10 +31,35 @@ public interface RobotProperties {
 
         /** CAN ID Properties **/
         public static final int GYRO_CAN_ID = 10;
+        public static final int LOWER_SHOOTER_CAN_ID = 20, UPPER_SHOOTER_CAN_ID = 21, SHOOTER_TILT_CAN_ID = 30;
+        public static final int LOWER_FEEDER_CAN_ID = 20, UPPER_FEEDER_CAN_ID = 21;
+
+        /** Shooter Variables **/
+        public static final boolean LOWER_SHOOTER_INVERTED = false, UPPER_SHOOTER_INVERTED = true, SHOOTER_TILT_INVERTED = false;
+        public static final int LOWER_SHOOTER_VELOCITY = 1400, UPPER_SHOOTER_VELOCITY = 3800; // High shot
+        public static final int LOWER_SHOOTER_SHORT_VELOCITY = 500, UPPER_SHOOTER_SHORT_VELOCITY = 1925; // Low shot
+        public static final int LOWER_SHOOTER_YEET_VELOCITY = 3650, UPPER_SHOOTER_YEET_VELOCITY = 5350; // Yeet shot
+        public static final double DESIRED_PERCENT_ACCURACY = .08, DESIRED_PERCENT_ACCURACY_YEET = .05,
+                        DESIRED_AT_SPEED_TIME = .06, DESIRED_AT_SPEED_TIME_SHORT = .2; // Accuracy Settings
+
+        /** Pickup Variables **/
+        public static final int PICKUP_ARM_MAX_CURRENT = 90;
+        public static final double REVERSE_PICKUP_SPEED = -.5;
+        public static final double PICKUP_SPEED = .55;
+
+        /** Feeder Variables **/
+        public static final boolean LOWER_FEEDER_INVERTED = false, UPPER_FEEDER_INVERTED = true;
+        public static final double SHOOTER_LOWER_FEED_SPEED = .1, SHOOTER_UPPER_FEED_SPEED = .25;
+        public static final double REVERSE_LOWER_FEEDER_SPEED = -.75, REVERSE_UPPER_FEEDER_SPEED = -.75;
+        public static final double LOWER_FEEDER_SPEED = .3, LOWER_FEEDER_SPEED_SLOW = .2;
+        public static final double UPPER_FEEDER_SPEED = .2, UPPER_FEEDER_BACKFEED_SPEED = -.4;
+        public static final double LOWER_FEED_END_SPEED = .15, LOWER_FEED_END_TIME = .2;
+        public static final double UPPER_FEED_END_SPEED = -.3, UPPER_FEED_END_TIME = .2;
 
         /** PID Properties **/
         public static final double GYRO_KP = .013, GYRO_KI = .00075, GYRO_KD = .00075, GYRO_MIN = -.5, GYRO_MAX = .5;
         public static final double SLEW_KP = -.005, SLEW_KI = -.0004, SLEW_KD = .035, SLEW_KF = 0, SLEW_PID_MIN = -1, SLEW_PID_MAX = 1;
+        public static final double SHOOTER_KP = .01, SHOOTER_KI = .0002, SHOOTER_KD = .0001, SHOOTER_KF = 0;
 
         /** Auton Mode Constants **/
         public static final String DEFAULT_AUTON = "Disabled";
---
+++ src/main/java/frc/team3171/controllers/Shooter.java
@@ -0,0 +1,445 @@
+package frc.team3171.controllers;
+
+// Java Imports
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.ReentrantLock;
+
+// FRC Imports
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.TimedRobot;
+
+// REV Imports
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+// Team 3171 Imports
+import frc.robot.RobotProperties;
+
+/**
+ * @author Mark Ebert
+ */
+public class Shooter implements RobotProperties {
+
+    // Motor Controllers
+    private final CANSparkFlex lowerShooterMotor, upperShooterMotor;
+    private CANSparkMax lowerFeederMotor, upperFeederMotor, shooterTiltMotor;
+    // private final RelativeEncoder pickupArmEncoder;
+    // private final SparkMaxPIDController pickupArmPIDController;
+
+    // PID Controllers
+    private final SparkPIDController lowerShooterPIDController, upperShooterPIDController;
+    private volatile int lowerShooterTargetRPM, upperShooterTargetRPM;
+
+    // Executor Service
+    private final ExecutorService executorService;
+
+    // Reentrant Locks
+    private final ReentrantLock executorLock;
+
+    // Atomic Booleans
+    private final AtomicBoolean lowerFeederExecutorActive, upperFeederExecutorActive;
+
+    /**
+     * Constructor
+     * 
+     * @throws Exception
+     *             Throws a new exception if there are an invalid amount of
+     *             motors in the feederCANIDArray.
+     */
+    public Shooter() throws Exception {
+        // Init all of the motors
+        lowerShooterMotor = new CANSparkFlex(LOWER_SHOOTER_CAN_ID, MotorType.kBrushless);
+        upperShooterMotor = new CANSparkFlex(UPPER_SHOOTER_CAN_ID, MotorType.kBrushless);
+        lowerFeederMotor = new CANSparkMax(LOWER_FEEDER_CAN_ID, MotorType.kBrushless);
+        upperFeederMotor = new CANSparkMax(UPPER_FEEDER_CAN_ID, MotorType.kBrushless);
+        shooterTiltMotor = new CANSparkMax(SHOOTER_TILT_CAN_ID, MotorType.kBrushless);
+
+        // Set if any motors need to be inverted
+        lowerShooterMotor.setInverted(LOWER_SHOOTER_INVERTED);
+        upperShooterMotor.setInverted(UPPER_SHOOTER_INVERTED);
+        lowerFeederMotor.setInverted(LOWER_FEEDER_INVERTED);
+        upperFeederMotor.setInverted(UPPER_FEEDER_INVERTED);
+        shooterTiltMotor.setInverted(SHOOTER_TILT_INVERTED);
+
+        // Configure the velocity closed loop values
+        lowerShooterMotor.restoreFactoryDefaults();
+        lowerShooterPIDController = lowerShooterMotor.getPIDController();
+        lowerShooterPIDController.setP(SHOOTER_KP);
+        lowerShooterPIDController.setI(SHOOTER_KI);
+        lowerShooterPIDController.setD(SHOOTER_KD);
+        lowerShooterPIDController.setFF(SHOOTER_KF);
+        lowerShooterPIDController.setOutputRange(-1, 1);
+        // lowerShooterPIDController.setSmartMotionMaxVelocity(2000, 0);
+        // lowerShooterPIDController.setSmartMotionMinOutputVelocity(0, 0);
+        // lowerShooterPIDController.setSmartMotionMaxAccel(1500, 0);
+        lowerShooterMotor.burnFlash();
+        lowerShooterTargetRPM = 0;
+
+        upperShooterMotor.restoreFactoryDefaults();
+        upperShooterPIDController = upperShooterMotor.getPIDController();
+        upperShooterPIDController.setP(SHOOTER_KP);
+        upperShooterPIDController.setI(SHOOTER_KI);
+        upperShooterPIDController.setD(SHOOTER_KD);
+        upperShooterPIDController.setFF(SHOOTER_KF);
+        upperShooterPIDController.setOutputRange(-1, 1);
+        // upperShooterPIDController.setSmartMotionMaxVelocity(2000, 0);
+        // upperShooterPIDController.setSmartMotionMinOutputVelocity(0, 0);
+        // upperShooterPIDController.setSmartMotionMaxAccel(1500, 0);
+        upperShooterMotor.burnFlash();
+        upperShooterTargetRPM = 0;
+
+        // Initialize the executor service for concurrency
+        executorService = Executors.newFixedThreadPool(2);
+        executorLock = new ReentrantLock(true);
+
+        // Initialize the AtomicBooleans to control the thread executors
+        lowerFeederExecutorActive = new AtomicBoolean(false);
+        upperFeederExecutorActive = new AtomicBoolean(false);
+    }
+
+    /**
+     * Sets the speed of the shooter motors to the given value.
+     * 
+     * @param lowerShooterSpeed
+     *            The speed, from -1.0 to 1.0, to set the lower
+     *            shooter motor to.
+     * @param upperShooterSpeed
+     *            The speed, from -1.0 to 1.0, to set the upper
+     *            shooter motor to.
+     */
+    public void setShooterSpeed(final double lowerShooterSpeed, final double upperShooterSpeed) {
+        lowerShooterTargetRPM = 0;
+        upperShooterTargetRPM = 0;
+        lowerShooterMotor.set(lowerShooterSpeed);
+        upperShooterMotor.set(upperShooterSpeed);
+    }
+
+    /**
+     * Sets the speed of the shooter motors to the given value.
+     * 
+     * @param shooterSpeed
+     *            The speed, from -1.0 to 1.0, to set the all of the
+     *            shooter motors to.
+     */
+    public void setShooterSpeed(final double shooterSpeed) {
+        setShooterSpeed(shooterSpeed, shooterSpeed);
+    }
+
+    /**
+     * Sets the RPM of the shooter motors to the given value.
+     * 
+     * @param lowerShooterTargetRPM
+     *            The RPM to set the lower shooter motor to.
+     * @param upperShooterTargetRPM
+     *            The RPM to set the upper shooter motor to.
+     */
+    public void setShooterVelocity(final int lowerShooterTargetRPM, final int upperShooterTargetRPM) {
+        /**
+         * First check if either desired RPM is 0, if so lets the electronic brake
+         * handle the slow done rather then the PID Controller.
+         */
+        if (lowerShooterTargetRPM == 0) {
+            lowerShooterMotor.set(0);
+        } else if (this.lowerShooterTargetRPM != lowerShooterTargetRPM) {
+            lowerShooterPIDController.setReference(lowerShooterTargetRPM, ControlType.kVelocity);
+        }
+        this.lowerShooterTargetRPM = lowerShooterTargetRPM;
+        if (upperShooterTargetRPM == 0) {
+            upperShooterMotor.set(0);
+        } else if (this.upperShooterTargetRPM != upperShooterTargetRPM) {
+            upperShooterPIDController.setReference(upperShooterTargetRPM, ControlType.kVelocity);
+        }
+        this.upperShooterTargetRPM = upperShooterTargetRPM;
+    }
+
+    /**
+     * Sets the RPM of the shooter motors to the given value.
+     * 
+     * @param shooterRPM
+     *            The RPM to set the all of the shooter motors to.
+     */
+    public void setShooterVelocity(final int shooterRPM) {
+        setShooterVelocity(shooterRPM, shooterRPM);
+    }
+
+    /**
+     * Returns the velocity of the lower shooter motor in RPM, converted from Units
+     * per 100ms.
+     * 
+     * @return The RPM of the lower shooter motor.
+     */
+    public double getLowerShooterVelocity() {
+        return lowerShooterMotor.getEncoder().getVelocity();
+    }
+
+    /**
+     * Returns if the velocity of the motor is within the provided percent error
+     * margin.
+     * 
+     * @param percentError
+     *            The percent error, from 0.0 to 1.0 with 1.0 being
+     *            equivilent to 100%, allowed to be considered at velocity.
+     * @return true if the motors current velocity is within the given percent
+     *         error, false otherwise.
+     */
+    public boolean isLowerShooterAtVelocity(double percentError) {
+        percentError = Math.abs(percentError);
+        percentError = percentError > 1 ? 1.0 : percentError;
+        final double error = Math.abs(lowerShooterTargetRPM - getLowerShooterVelocity());
+        final double acceptableError = error * percentError;
+        return error < acceptableError;
+    }
+
+    /**
+     * Returns the velocity of the upper shooter motor in RPM, converted from Units
+     * per 100ms.
+     * 
+     * @return The RPM of the upper shooter motor.
+     */
+    public double getUpperShooterVelocity() {
+        return upperShooterMotor.getEncoder().getVelocity();
+    }
+
+    /**
+     * Returns if the velocity of the motor is within the provided percent error
+     * margin.
+     * 
+     * @param percentError
+     *            The percent error, from 0.0 to 1.0 with 1.0 being
+     *            equivilent to 100%, allowed to be considered at velocity.
+     * @return true if the motors current velocity is within the given percent
+     *         error, false otherwise.
+     */
+    public boolean isUpperShooterAtVelocity(double percentError) {
+        percentError = Math.abs(percentError);
+        percentError = percentError > 1 ? 1.0 : percentError;
+        final double error = Math.abs(upperShooterTargetRPM - getUpperShooterVelocity());
+        final double acceptableError = error * percentError;
+        return error < acceptableError;
+    }
+
+    /**
+     * Returns the target velocity of the lower shooter motor in RPM, converted from
+     * Units per 100ms.
+     * 
+     * @return The target RPM of the lower shooter motor.
+     */
+    public double getLowerShooterTargetVelocity() {
+        return lowerShooterTargetRPM;
+    }
+
+    /**
+     * Returns the target velocity of the upper shooter motor in RPM, converted from
+     * Units per 100ms.
+     * 
+     * @return The RPM of the upper shooter motor.
+     */
+    public double getUpperShooterTargetVelocity() {
+        return upperShooterTargetRPM;
+    }
+
+    /**
+     * Returns the speed of the lower shooter motor in percent.
+     * 
+     * @return The speed of the lower shooter motor, from -1.0 to 1.0.
+     */
+    public double getLowerShooterSpeed() {
+        return lowerShooterMotor.get();
+    }
+
+    /**
+     * Returns the speed of the upper shooter motor.
+     * 
+     * @return The speed of the upper shooter motor in percent, from -1.0 to 1.0.
+     */
+    public double getUpperShooterSpeed() {
+        return upperShooterMotor.get();
+    }
+
+    /**
+     * Returns if the velocity of the both shooter motors is within the
+     * provided percent error margin.
+     * 
+     * @param percentError
+     *            The percent error, from 0.0 to 1.0 with 1.0 being
+     *            equivilent to 100%, allowed to be considered at velocity.
+     * @return true if the motors current velocity is within the given percent
+     *         error, false otherwise.
+     */
+    public boolean isBothShootersAtVelocity(double percentError) {
+        percentError = Math.abs(percentError);
+        percentError = percentError > 1 ? 1.0 : percentError;
+        final double upperError = Math.abs(upperShooterTargetRPM - getLowerShooterVelocity());
+        final double lowerError = Math.abs(lowerShooterTargetRPM - getLowerShooterVelocity());
+        final double upperAcceptableError = upperError * percentError;
+        final double lowerAcceptableError = lowerError * percentError;
+        return upperError < upperAcceptableError && lowerError < lowerAcceptableError;
+    }
+
+    /**
+     * Sets the speed of the feeder motors to the given value.
+     * 
+     * @param speed
+     *            The speed, from -1.0 to 1.0, to set the feeder motors to.
+     */
+    public void setLowerFeederSpeed(final double speed) {
+        if (!lowerFeederExecutorActive.get()) {
+            // lowerFeederMotor.set(speed);
+        }
+    }
+
+    /**
+     * Sets the speed of the feeder motors to the given value and keeps them running
+     * for the desired time.
+     * 
+     * @param speed
+     *            The speed, from -1.0 to 1.0, to set the feeder motor to.
+     * @param runTime
+     *            The amount of time, in seconds, to keep the motors spinning
+     *            for.
+     */
+    public void runLowerFeeder(final double speed, final double runTime) {
+        try {
+            executorLock.lock();
+            if (lowerFeederExecutorActive.compareAndSet(false, true)) {
+                executorService.execute(() -> {
+                    try {
+                        final double endTime = Timer.getFPGATimestamp() + runTime;
+                        while (Timer.getFPGATimestamp() <= endTime) {
+                            if (DriverStation.isDisabled()) {
+                                break;
+                            }
+                            // lowerFeederMotor.set(ControlMode.PercentOutput, speed);
+                            Timer.delay(TimedRobot.kDefaultPeriod);
+                        }
+                        // lowerFeederMotor.set(ControlMode.PercentOutput, 0);
+                    } finally {
+                        lowerFeederExecutorActive.set(false);
+                    }
+                });
+            }
+        } finally {
+            executorLock.unlock();
+        }
+    }
+
+    /**
+     * Sets the speed of the feeder motors to the given value.
+     * 
+     * @param speed
+     *            The speed, from -1.0 to 1.0, to set the feeder motors to.
+     */
+    public void setUpperFeederSpeed(final double speed) {
+        if (!upperFeederExecutorActive.get()) {
+            // upperFeederMotor.set(ControlMode.PercentOutput, speed);
+        }
+    }
+
+    /**
+     * Sets the speed of the feeder motors to the given value and keeps them running
+     * for the desired time.
+     * 
+     * @param speed
+     *            The speed, from -1.0 to 1.0, to set the feeder motor to.
+     * @param runTime
+     *            The amount of time, in seconds, to keep the motors spinning
+     *            for.
+     */
+    public void runUpperFeeder(final double speed, final double runTime) {
+        try {
+            executorLock.lock();
+            if (upperFeederExecutorActive.compareAndSet(false, true)) {
+                executorService.execute(() -> {
+                    try {
+                        final double endTime = Timer.getFPGATimestamp() + runTime;
+                        while (Timer.getFPGATimestamp() <= endTime) {
+                            if (DriverStation.isDisabled()) {
+                                break;
+                            }
+                            // upperFeederMotor.set(ControlMode.PercentOutput, speed);
+                            Timer.delay(TimedRobot.kDefaultPeriod);
+                        }
+                        // upperFeederMotor.set(ControlMode.PercentOutput, 0);
+                    } finally {
+                        upperFeederExecutorActive.set(false);
+                    }
+                });
+            }
+        } finally {
+            executorLock.unlock();
+        }
+    }
+
+    /**
+     * Sets the speed of the feeder motors to the given value and keeps them running
+     * for the desired time.
+     * 
+     * @param speed
+     *            The speed, from -1.0 to 1.0, to set the feeder motor to.
+     * @param runTime
+     *            The amount of time, in seconds, to keep the motors spinning
+     *            for.
+     */
+    public void pulseUpperFeeder(final double speed, final double runTime) {
+        try {
+            executorLock.lock();
+            if (upperFeederExecutorActive.compareAndSet(false, true)) {
+                executorService.execute(() -> {
+                    try {
+                        double endTime = Timer.getFPGATimestamp() + runTime;
+                        while (Timer.getFPGATimestamp() <= endTime) {
+                            if (DriverStation.isDisabled()) {
+                                break;
+                            }
+                            // upperFeederMotor.set(ControlMode.PercentOutput, speed);
+                            Timer.delay(.02);
+                        }
+                        endTime = Timer.getFPGATimestamp() + runTime;
+                        while (Timer.getFPGATimestamp() <= endTime) {
+                            if (DriverStation.isDisabled()) {
+                                break;
+                            }
+                            // upperFeederMotor.set(ControlMode.PercentOutput, 0);
+                            Timer.delay(.02);
+                        }
+                        // upperFeederMotor.set(ControlMode.PercentOutput, 0);
+                    } finally {
+                        upperFeederExecutorActive.set(false);
+                    }
+                });
+            }
+        } finally {
+            executorLock.unlock();
+        }
+    }
+
+    /**
+     * Sets the speed of the pickup motor to the given value.
+     * 
+     * @param speed
+     *            The speed, from -1.0 to 1.0, to set the pickup motors to.
+     */
+    public void setPickupSpeed(final double speed) {
+        // pickupMotor.set(ControlMode.PercentOutput, speed);
+    }
+
+    /**
+     * Disables all motors in the {@link Shooter} class.
+     */
+    public final void disable() {
+        lowerShooterTargetRPM = 0;
+        upperShooterTargetRPM = 0;
+        lowerShooterMotor.disable();
+        upperShooterMotor.disable();
+        // pickupMotor.set(ControlMode.Disabled, 0);
+        // lowerFeederMotor.set(ControlMode.Disabled, 0);
+        // upperFeederMotor.set(ControlMode.Disabled, 0);
+    }
+
+}
