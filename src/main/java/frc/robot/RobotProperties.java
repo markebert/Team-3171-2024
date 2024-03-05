30a3b9992c6ce8a5ca05ce651d3bdc7ad37080ea
Jdfletch <118640277+Jdfletch@users.noreply.github.com>
2024-03-05 16:42:12 -0500

    swapped amp shot speeds

--- src/main/java/frc/robot/RobotProperties.java
+++ src/main/java/frc/robot/RobotProperties.java
@@ -59,7 +59,7 @@ public interface RobotProperties {
         public static final double SHOOTER_REVERSE_FEED_SPEED = -.5;
         public static final HashMap<String, ShooterShot> SHOOTER_SHOTS = new HashMap<>() {
                 {
-                        put("SHORT_SHOT", new ShooterShot(45, 300, 2000));
+                        put("SHORT_SHOT", new ShooterShot(40, 2000, 300));
                         put("NORMAL_SHOT", new ShooterShot(37, 2500, 3000));
                         put("FAR_SHOT", new ShooterShot(59, 4000, 4000));
                         put("YEET_SHOT", new ShooterShot(25, 5500, 5500));
