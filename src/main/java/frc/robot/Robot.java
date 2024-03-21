3e3e21c7e1e22ab3769f2b093d463acf6812d511
Jdfletch <118640277+Jdfletch@users.noreply.github.com>
2024-03-21 17:47:58 -0400

    changed layover button

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -439,9 +439,9 @@ public class Robot extends TimedRobot implements RobotProperties {
     final double fieldCorrectedAngle = FIELD_ORIENTED_SWERVE ? Normalize_Gryo_Value(leftStickAngle - gyroValue) : leftStickAngle;
 
     // Layover button
-    final boolean button_layover = driveControllerState.getXButton();
+    final boolean button_layover = operatorControllerState.getLeftBumper();
     if (button_layover) {
-      shooterController.setShooterTiltPosition(-60);
+      shooterController.setShooterTiltPosition(60);
       doLayoverShot = true;
     }
 
